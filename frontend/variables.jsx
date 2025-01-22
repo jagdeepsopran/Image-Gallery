816facd308fa63663ddaaad1cfe605d2c2098166
Jagdeep <jagdeepsopran@gmail.com>
2025-01-22 22:37:07 +0530

    second

--- frontend/variables.jsx
+++ frontend/variables.jsx
@@ -1,3 +1,3 @@
-// export const baseURL = "https://full-stack-to-do-app-g8h5.onrender.com";
-export const baseURL = "http://localhost:6700";
+export const baseURL = "https://image-gallery-pom2.onrender.com";
+// export const baseURL = "http://localhost:6700";
 
